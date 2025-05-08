09a0a42e3af3260e5a235c88108718c84d055e10
Jonas Tomaz <jonas.aquinos@hotmail.com>
2025-05-08 15:24:12 -0300

    fix: import authService

--- frontend/src/stores/auth.js
+++ frontend/src/stores/auth.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia';
-import authService from '../services/auth.service';
+import authService from '../services/authService';
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
